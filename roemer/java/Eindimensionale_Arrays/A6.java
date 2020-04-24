db4e83be1358f4a2091ecb0de8179a4cfa6e278a
DvTox <mail@example.com>
2020-04-24 09:56:48 +0200

    added dupe detection in A6

--- roemer/java/Eindimensionale_Arrays/A6.java
+++ roemer/java/Eindimensionale_Arrays/A6.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
 
 public class A6{
+    static Random rdm = new Random();
     public static void main(String[] args){
-        Random rdm = new Random();
+        
 
         int[] lottoZahlen = new int[6];
         int[] rateZahlen = new int[6];
@@ -23,8 +25,8 @@ public class A6{
         System.out.printf("%n");
 
         System.out.printf("%nDie Lottozahlen: ");
+        lottoZahlen = generateRandomNumbers(0, 49, lottoZahlen);
         for (int i = 0; i < lottoZahlen.length; i++) {
-            lottoZahlen[i] = (rdm.nextInt(49) + 1);
             System.out.printf("%d ",lottoZahlen[i]);
         }
         System.out.printf("%n");
@@ -36,5 +38,28 @@ public class A6{
         }
         System.out.printf("Anzahl der Treffer : %d%n",treffer);
     }
-}
-    
+
+    public static boolean checkForDupes(int[] arr) {
+        
+        for (int o = 0; o < arr.length; o++) {
+            for (int y = 0; y < (arr.length -1); y++) {
+                if(o == y){
+                    continue;
+                }else if (arr[o] == arr[y]) {
+                   return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int[] generateRandomNumbers(int min, int max, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rdm.nextInt((max -min +1) -min);
+        }
+        if (checkForDupes(arr) == false) {
+            generateRandomNumbers(min, max, arr);
+        }
+        return(arr);
+    }
+}    
